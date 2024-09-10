fa978fcddbfed9d31ea761fb12973c1f7ebec514
reduxscripts <pals.sten@gmail.com>
2024-09-10 17:44:51 +0300

    leak

---
+++ base.sql
@@ -0,0 +1,48 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               11.5.2-MariaDB-log - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for redm
+CREATE DATABASE IF NOT EXISTS `redm` /*!40100 DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci */;
+USE `redm`;
+
+-- Dumping structure for table redm.community_users
+CREATE TABLE IF NOT EXISTS `community_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hex_id` varchar(100) DEFAULT NULL,
+  `steam_id` varchar(50) DEFAULT NULL,
+  `community_id` varchar(100) DEFAULT NULL,
+  `license` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL DEFAULT 'Uknown',
+  `ip` varchar(50) NOT NULL DEFAULT 'Uknown',
+  `rank` varchar(50) NOT NULL DEFAULT 'user',
+  `date_created` timestamp NULL DEFAULT current_timestamp(),
+  `controls` text DEFAULT '{}',
+  `settings` text DEFAULT '{}',
+  `inventory_settings` longtext DEFAULT NULL,
+  `tunnid` bigint(20) DEFAULT NULL,
+  `hours` bigint(20) DEFAULT NULL,
+  `wlstatus` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
