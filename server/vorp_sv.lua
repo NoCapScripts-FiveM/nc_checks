baff759106bc244555d70d64706b24451b38c721
nocapscripts <pals.sten@gmail.com>
2024-10-30 21:54:02 +0200

    update

--- server/vorp_sv.lua
+++ server/vorp_sv.lua
@@ -15,53 +15,57 @@ end)
 -- ==============================
 -- Player Connecting Logic for RedM
 -- ==============================
-if Framework == 'VORP' then 
-    VORP = exports.vorp_core:GetCore()
+CreateThread(function()
+    Wait(10)
 
-    print("NC CHECKS")
-    function onPlayerConnecting(name, setKickReason, deferrals)
-        deferrals.defer()
+    if Framework == 'VORP' then 
+        VORP = exports.vorp_core:GetCore()
 
-        local src = source
-        if not src then
-            deferrals.done("Error: Source not found.")
-            return
-        end
+        print("NC CHECKS")
+        function onPlayerConnecting(name, setKickReason, deferrals)
+            deferrals.defer()
 
-        local self = {
-            source = src,
-            name = GetPlayerName(src),
-            hexid = Checks.Util:GetHexId(src),
-            license = Checks.Util:GetLicense(src)
-        }
-
-        print(string.format(
-            "\n\27[34m[INFO]\27[0m Player Data:\n" ..
-            "\27[32mName:\27[0m %s\n" ..
-            "\27[32mHexID:\27[0m %s\n" ..
-            "\27[32mLicense:\27[0m %s\n" ..
-            "\27[33mPlayer %s is joining the server...\27[0m",
-            self.name, self.hexid, self.license, self.name
-        ))
-
-        -- User Check
-        if Config.UserCheck then
-            for i = 1, 3 do
-                deferrals.update('Kasutajate kontroll: ' .. i .. '/3.')
-                deferrals.update('Kontrollime teie andmeid: '.. self.name)
-                Citizen.Wait(1000)
+            local src = source
+            if not src then
+                deferrals.done("Error: Source not found.")
+                return
             end
-            Checks.User.CreateNewUser(self.source)
+
+            local self = {
+                source = src,
+                name = GetPlayerName(src),
+                hexid = Checks.Util:GetHexId(src),
+                license = Checks.Util:GetLicense(src)
+            }
+
+            print(string.format(
+                "\n\27[34m[INFO]\27[0m Player Data:\n" ..
+                "\27[32mName:\27[0m %s\n" ..
+                "\27[32mHexID:\27[0m %s\n" ..
+                "\27[32mLicense:\27[0m %s\n" ..
+                "\27[33mPlayer %s is joining the server...\27[0m",
+                self.name, self.hexid, self.license, self.name
+            ))
+
+            -- User Check
+            if Config.UserCheck then
+                for i = 1, 3 do
+                    deferrals.update('Kasutajate kontroll: ' .. i .. '/3.')
+                    deferrals.update('Kontrollime teie andmeid: '.. self.name)
+                    Citizen.Wait(1000)
+                end
+                Checks.User.CreateNewUser(self.source)
+            end
+
+            if Config.Logs then
+                exports.nc_logs:AddLog("JOIN", self.hexid, "Player joined the server", nil)
+            end
+            deferrals.done()
         end
 
-        if Config.Logs then
-            exports.nc_logs:AddLog("JOIN", self.hexid, "Player joined the server", nil)
-        end
-        deferrals.done()
+        AddEventHandler('playerConnecting', onPlayerConnecting)
     end
-
-    AddEventHandler('playerConnecting', onPlayerConnecting)
-end 
+end)
 
 
 
