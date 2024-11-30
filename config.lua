945aca1a151b28133113e7f5b040ca5a534ee9ec
nocapscripts <pals.sten@gmail.com>
2024-11-30 19:43:57 +0200

    Cfg desc

--- config.lua
+++ config.lua
@@ -1,22 +1,22 @@
 Config = Config or {}
 
-Config.Logs = true -- Default: false
+Config.Logs = true -- Default: false [Enables logs feature with depedency resource]
 
 -- Core Settings
-Config.IdentifierType = 'steam'
-Config.ESXCoreName = 'ncfw'
-Config.QBCoreName = 'nc_fw' -- Default: qb-core or else
-Config.VORPCoreName = "vorp_core"
+Config.IdentifierType = 'steam' --[Select identifier type]
+Config.ESXCoreName = 'ncfw' --[Select Framework name for ESX]
+Config.QBCoreName = 'nc_fw' --[Select Framework name for QBUS]
+Config.VORPCoreName = "vorp_core" --[Select Framework name for REDM VORP]
 
--- Feature Toggles
-Config.RedM = false
+-- Gametype Feature Toggles
+Config.RedM = false --[Select GameType for CFX]
 Config.Fivem = true
 
 -- Checks
-Config.UserCheck = true
-Config.SavePlayersHours = true
-Config.Whitelist = false
-Config.NameCheck = true
-Config.Discord = false
-Config.Identifier = true
-Config.Ban = true
+Config.UserCheck = true --[User account creation and data check]
+Config.SavePlayersHours = true --[User playhours ] IDK why is this here
+Config.Whitelist = false --[User whitelist check ]
+Config.NameCheck = true --[User name check ]
+Config.Discord = false --[User discord check ]
+Config.Identifier = true --[User license check ]
+Config.Ban = true --[User ban check ]
